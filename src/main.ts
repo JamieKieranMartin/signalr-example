20475a7e063511df80865714d6b5f2e79b05154c
Jamie Martin <jamie.kieran.martin@gmail.com>
2021-02-25 17:06:24 +1000

    :construction:

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,34 @@
+import * as signalR from '@microsoft/signalr';
+
 const main = async () => {
-  console.log('Time to get started!');
+  const connection = new signalR.HubConnectionBuilder()
+    .configureLogging(signalR.LogLevel.Debug)
+    .withUrl(`https://localhost:6001/hubs/log`, {
+      skipNegotiation: true,
+      transport: signalR.HttpTransportType.WebSockets
+    })
+    .withAutomaticReconnect()
+    .build();
+
+  connection.on('logs', (data) => {
+    console.log(data);
+  });
+
+  connection
+    .start()
+    .then(() => console.log('Log Service Connected'))
+    .catch((e) => console.log(e));
+  // .then(() =>
+  // connection.invoke('log', {
+  //   // time: new Date().toISOString(),
+  //   // action: '123',
+  //   // completedSuccessfully: true,
+  //   // deviceId: 'sflkjsdfkldf',
+  //   // returnValue: '200'
+  // })
+  // )
+
+  connection.onclose(console.log);
 };
 
 main().catch((err) => console.error(err));
